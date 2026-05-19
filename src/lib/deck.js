dd599123417ab0344b35cdfbb35d78d387f9c140
sabrinahaniff <xxcrystalwindxx@gmail.com>
2026-05-19 15:40:39 -0400

    create actual deck

--- src/lib/deck.js
+++ src/lib/deck.js
@@ -2,3 +2,13 @@ export const SUITS = ['♠', '♥', '♦', '♣'];
 export const VALUES = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 export const RED_SUITS = ['♥', '♦'];
 
+export function createDeck() {
+  const deck = [];
+  for (const suit of SUITS) {
+    for (const value of VALUES) {
+      deck.push({ suit, value, id: `${value}${suit}` });
+    }
+  }
+  return deck;
+}
+
